9a2a42837013aafec6606124c88c1570d8b9d407
rrich-kray <rrich.kray@gmail.com>
2022-07-01 19:11:24 -0400

    prep for heroku deployment

--- server/server.js
+++ server/server.js
@@ -11,6 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}`);
